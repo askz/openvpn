b917a4eab87bda988ed7022d8a440d9b4a022c44
Max <854038+askz@users.noreply.github.com>
2018-10-01 22:08:50 +0200

    Update apt-repo.rb

--- recipes/apt-repo.rb
+++ recipes/apt-repo.rb
@@ -6,5 +6,6 @@
 apt_repository 'openvpn' do
   uri        'http://build.openvpn.net/debian/openvpn/stable/'
   key        'https://swupdate.openvpn.net/repos/repo-public.gpg'
+  distribution 'stretch'
   components ['main']
 end
